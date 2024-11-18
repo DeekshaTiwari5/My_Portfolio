4df81a7a2e9a5c1901e64f7d36ff4eed416a22e9
Harshit Tiwari <72160494+HarshitTiwari-20@users.noreply.github.com>
2024-11-18 15:19:03 +0530

    Initial commit

---
+++ src/app/data.js
@@ -0,0 +1,121 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "iPhone-16-site",
+    description: "Displaying the features of newly launched iPhone",
+    date: "2024-08-31",
+    demoLink: "https://iphone-16-site.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "AI-Image Generator",
+    description: "Digital image generator for the given prompt",
+    date: "2024-09-09",
+    demoLink: "https://flourishing-lamington-e72362.netlify.app",
+  },
+  {
+    id: 3,
+    name: "AI Voice Assistant",
+    description: "Responde to asked Questions",
+    date: "2024-09-15",
+    demoLink: " https://deekshatiwari5.github.io/VoiceAssistance/",
+  },
+  {
+    id: 4,
+    name: "HealthBeat",
+    description: "Monitor heart rate zones",
+    date: "2022-05-30",
+    demoLink: "https://healthbeat.example.com",
+  },
+  {
+    id: 5,
+    name: "RecipeFinder",
+    description: "Discover new recipes",
+    date: "2023-07-12",
+    demoLink: "https://deekshatiwari5.github.io/MEAL-APP/",
+  },
+  {
+    id: 6,
+    name: "Coffee Shop",
+    description: "creative coffee shop",
+    date: "2024-11-16",
+    demoLink: "https://glittery-mandazi-caae8a.netlify.app/",
+  },
+  {
+    id: 7,
+    name: "StudyBuddy",
+    description: "Collaborative learning platform",
+    date: "2022-04-18",
+    demoLink: "https://studybuddy.example.com",
+  },
+  {
+    id: 8,
+    name: "TechTalk",
+    description: "Tech news aggregator",
+    date: "2022-11-05",
+    demoLink: "https://techtalk.example.com",
+  },
+  {
+    id: 9,
+    name: "FitTrack",
+    description: "Fitness and workout tracker",
+    date: "2022-03-22",
+    demoLink: "https://fittrack.example.com",
+  },
+  {
+    id: 10,
+    name: "MindfulMoments",
+    description: "Meditation and mindfulness app",
+    date: "2022-02-14",
+    demoLink: "https://mindfulmoments.example.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/DeekshaTiwari5",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/deeksha-tiwari-a6a12b260/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Leetcode",
+    link: "https://leetcode.com/u/tdeeksha617",
+    icon: "leetcode",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
